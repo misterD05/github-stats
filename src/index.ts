955319f32e0813c128294f2082d437ba41d0da8a
misterD05 <mrD05.dev@protonmail.com>
2026-08-07 14:29:51 +0200

    another try for deployment

--- src/index.ts
+++ src/index.ts
@@ -101,9 +101,3 @@ app.get('/api/hexagonalStats/:username/:color', async (c) => {
 
 export default app;
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
